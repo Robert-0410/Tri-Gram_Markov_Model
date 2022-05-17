c4899aac3d02d1dc03f37ac95ebad3e2a6a9d2ef
Robert-0410 <62630290+Robert-0410@users.noreply.github.com>
2022-05-17 15:04:22 -0700

    Work form 5/17, continue to work on split_by_period()

--- Tester.py
+++ Tester.py
@@ -1,5 +1,22 @@
 # Reads input files, connects to Markov.py to generate the output story
 
+# TODO data structure that will hold words / phrases
+
+# Split sentences by period and returns a list with all the sentences TODO skip over title, author, and chapters
+def split_by_period():
+    delimiters = '.!?'
+    lines = list()
+    novel = open('houn.txt', 'r')
+
+    for i in range(15):  # TODO must parse entire file
+        next_line = novel.readline().rstrip('\n')
+        lines.append(next_line)
+
+    novel.close()
+    print(lines)
+    return 0  # TODO temp return
+
+
 # removes punctuation from an input sentence
 # inspired by: https://www.geeksforgeeks.org/removing-punctuations-given-string/
 def remove_punctuation(sentence: str):
@@ -11,5 +28,8 @@ def remove_punctuation(sentence: str):
     return sentence
 
 
-string = "Welcome???@@##$ to#$% Geeks%$^for$%^&Geeks"
+string = "Welcome???@@##$ to#$% Geeks%$^for$%^&Geeks. You going to learn today! Are you ready? YOu better be."
+string2 = "welcome???@@##$ to#$% geeks%$^for$%^&geeks"
 print(remove_punctuation(string))
+print(remove_punctuation(string2))
+split_by_period()
