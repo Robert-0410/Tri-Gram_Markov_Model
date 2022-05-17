ce8e40bd5145578aa1594e46b9271cba8a9320b2
Robert-0410 <62630290+Robert-0410@users.noreply.github.com>
2022-05-16 17:21:50 -0700

    implemented function to remove punctuation

--- Tester.py
+++ Tester.py
@@ -1,1 +1,15 @@
 # Reads input files, connects to Markov.py to generate the output story
+
+# removes punctuation from an input sentence
+# inspired by: https://www.geeksforgeeks.org/removing-punctuations-given-string/
+def remove_punctuation(sentence: str):
+    # Regular expression
+    punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+    for x in sentence.lower():
+        if x in punctuations:
+            sentence = sentence.replace(x, "")
+    return sentence
+
+
+string = "Welcome???@@##$ to#$% Geeks%$^for$%^&Geeks"
+print(remove_punctuation(string))
