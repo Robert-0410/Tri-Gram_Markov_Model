d72eb61611d6a11cdc81a9531fd05fd7a4d5b763
Robert-0410 <62630290+Robert-0410@users.noreply.github.com>
2022-05-24 10:14:29 -0700

    read line 30 in Markov.py to continue

--- Markov.py
+++ Markov.py
@@ -4,20 +4,38 @@
 # where p1 is W_i-1
 #       p2 is W_i
 class Word:
-    def __init__(self, word):
-        self.word = word
+    def __init__(self, word: str):
+        self.word_data = word
         self.next_word1 = None  # other word that comes after the key/hashed
         self.next_word2 = None  # proceeding in a sentence
         self.is_i = False  # determines if 3rd word or position 2
 
+    def __eq__(self, other):
+        return self.word_data == other.word_data
+
+    # TODO delete if obsolete
     def add_word2(self, word2):
         self.next_word2 = word2
         self.next_word2.is_i = True
 
 
 class LinkedWords:
-    def __init__(self, word: Word):
-        self.head = word
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def add_word1(self, word1: Word):
+        return 0  # temp
+
+    # TODO this works for w1, refactor and make new onw for w2
+    def search(self, word: Word):
+        current = self.head
+
+        while current is not None:
+            if current == word:
+                return True
+            current = current.next_word1
+        return False
 
 
 # Tri-Gram Markov Model that helps AI create a new story
@@ -46,12 +64,12 @@ for sentence in test_list:
         current_word = s_to_list[i]
         # if key exist, update the linked list
         if current_word in the_dictionary:
-            current = the_dictionary.get(current_word)
+            current_list = the_dictionary.get(current_word)
             next_word = s_to_list[i + 1]
-            if next_word == current.head.word:
+            if next_word == current_list.head.word_data:
                 next_word2 = s_to_list[i + 2]
                 # need to add next_word2 as link or increase count of occurrence
-                if next_word2 == current.head.next_word2.word:
+                if next_word2 == current_list.head.next_word2.word_data:
                     print("increase count")
                 else:
                     print("move to next p2")
