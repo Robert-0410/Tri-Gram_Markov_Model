7cb29a1c12168382447b862af526345afd1c43cb
Robert-0410 <62630290+Robert-0410@users.noreply.github.com>
2022-05-26 21:19:56 -0700

    count is complete now work on making the story

--- Markov.py
+++ Markov.py
@@ -14,7 +14,7 @@ class Node:
     def __eq__(self, other):
         return self.data == other
 
-    def count(self):
+    def count_word(self):
         self.count += 1
 
     def add_layer2(self):
@@ -51,6 +51,9 @@ class LinkedWords:
         current = self.head
         while current is not None:
             print(current.data, end=" ")
+            layer = current.layer2
+            if layer is not None:
+                layer.to_string()
             current = current.next
 
 
@@ -58,6 +61,53 @@ class LinkedWords:
 class MarkovModel:
     def __init__(self):
         self.hash_table = dict()
+
+    def process_sentence(self, sentence: str):
+        s_list = sentence.split()
+        for i in range(len(s_list) - 2):  # minus 2 prevents index out of bounce
+            current_key = s_list[i]
+            word1 = s_list[i + 1]
+            word2 = s_list[i + 2]
+            # if key exist, update the linked list
+            if current_key in self.hash_table:
+                # layer refers to the linked lists
+                layer1 = self.hash_table.get(current_key)
+                response = layer1.search(word1)
+                # word is found, go into layer 2
+                if response[0]:
+                    layer1_node = response[1]
+                    layer1_node.count_word()
+                    #  print(response[1].data, " was found need to increment")
+                    layer2 = layer1_node.layer2
+                    response2 = layer2.search(word2)
+                    # word2 is found in layer 2
+                    if response2[0]:
+                        response2[1].count_word()
+                    # word2 not found in layer 2
+                    else:
+                        response[1].layer2.add_node(Node(word2).count_word())
+                # word is not found
+                else:
+                    node = Node(word1)
+                    node.count_word()
+                    node.add_layer2()
+                    node.layer2.add_node(Node(word2).count_word())
+                    layer1.add_node(node)
+
+            # if key does not exist, add as key and start layer 1 linked list
+            else:
+                layer1 = LinkedWords()
+                node = Node(word1)
+                node.count_word()
+                node.add_layer2()
+                # add W_i to layer 2
+                node.layer2.add_node(Node(word2).count_word())
+                layer1.add_node(node)
+                self.hash_table[current_key] = layer1
+
+    def train_markov_model(self, sentences: list):
+        for sentence in sentences:
+            self.process_sentence(sentence)
 
 
 # ---------------Code below is for testing and experimenting-------------------------
@@ -74,53 +124,9 @@ def test_sentences():
 
 
 test_list = test_sentences()
+story_teller = MarkovModel()
+story_teller.train_markov_model(test_list)
 
-# TODO idea: function that uses the below function to create the hash table, should take in a list of sentences
-# takes a sentence and hashes TODO: prevents sentence under size 3
-table = dict()
-for sentence in test_list:
-    # TODO idea: can have a function doing this work when taking a sentence as input
-    s_list = sentence.split()
-    for i in range(len(s_list) - 2):  # minus 2 prevents index out of bounce
-        current_key = s_list[i]
-        word1 = s_list[i + 1]
-        word2 = s_list[i + 2]
-        # if key exist, update the linked list
-        if current_key in table:
-            # layer refers to the linked lists
-            layer1 = table.get(current_key)
-            response = layer1.search(word1)
-            # word is found, go into layer 2
-            if response[0]:
-                print(response[1].data, " was found need to increment")
-                layer2 = response[1].layer2
-
-                response2 = layer2.search(word2)
-                # word2 is found in layer 2
-                if response2[0]:
-                    print("increment respective layer 2 node")
-                # word2 not found in layer 2
-                else:
-                    response[1].layer2.add_node(Node(word2))
-            # word is not found
-            else:
-                node = Node(word1)
-                node.add_layer2()
-                node.layer2.add_node(Node(word2))
-                layer1.add_node(node)
-
-        # if key does not exist, add as key and start layer 1 linked list
-        else:
-            # TODO continue with layer 2
-            layer1 = LinkedWords()
-            node = Node(word1)
-            node.add_layer2()
-            # add W_i to layer 2
-            node.layer2.add_node(Node(word2))
-            layer1.add_node(node)
-            table[current_key] = layer1
-
-print("Table size: ", len(table))
-for key in table:
+for key in story_teller.hash_table:
     print("The key: ", key, ": ", end=" ")
-    print(table.get(key).to_string())
+    print(story_teller.hash_table.get(key).to_string())
